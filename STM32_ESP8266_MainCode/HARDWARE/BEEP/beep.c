45a1c692812a30a0bb76cf8633428e3a3ee6c02f
SunJiYi 666 <53560510+a233a2@users.noreply.github.com>
2022-01-14 11:08:41 +0800

    Create beep.c

---
+++ STM32_ESP8266_MainCode/HARDWARE/BEEP/beep.c
@@ -0,0 +1,20 @@
+#include "beep.h"
+   
+
+//��ʼ��PB8Ϊ�����.��ʹ������ڵ�ʱ��		    
+//��������ʼ��
+void BEEP_Init(void)
+{
+ 
+ GPIO_InitTypeDef  GPIO_InitStructure;
+ 	
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	 //ʹ��GPIOB�˿�ʱ��
+ 
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;				 //BEEP-->PB.8 �˿�����
+ GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //�������
+ GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	 //�ٶ�Ϊ50MHz
+ GPIO_Init(GPIOB, &GPIO_InitStructure);	 //���ݲ�����ʼ��GPIOB.8
+ 
+ GPIO_ResetBits(GPIOB,GPIO_Pin_8);//���0���رշ��������
+
+}
