2725297257595abc6c77ca792d5d6383fff7a1f3
SunJiYi 666 <53560510+a233a2@users.noreply.github.com>
2022-01-14 11:19:00 +0800

    Create timer.c

---
+++ STM32_ESP8266_MainCode/HARDWARE/TIMER/timer.c
@@ -0,0 +1,210 @@
+#include "timer.h"
+#include "led.h"
+#include "stm32f10x_tim.h"
+#include "math.h"
+#include "led.h"
+#include "delay.h"
+#include "RC522.h"
+
+//void TIM4_Int_Init(u16 arr,u16 psc)
+//{
+//    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+//	NVIC_InitTypeDef NVIC_InitStructure;
+
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE); //ʱ��ʹ��
+
+//	TIM_TimeBaseStructure.TIM_Period = arr; //��������һ�������¼�װ�����Զ���װ�ؼĴ������ڵ�ֵ	 ������5000Ϊ500ms
+//	TIM_TimeBaseStructure.TIM_Prescaler =psc; //����������ΪTIMxʱ��Ƶ�ʳ�����Ԥ��Ƶֵ  10Khz�ļ���Ƶ��  
+//	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //����ʱ�ӷָ�:TDTS = Tck_tim
+//	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM���ϼ���ģʽ
+//	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure); //����TIM_TimeBaseInitStruct��ָ���Ĳ�����ʼ��TIMx��ʱ�������λ
+// 
+//	TIM_ITConfig( TIM4, TIM_IT_Update ,ENABLE  );//TIM2  //ʹ�ܻ���ʧ��ָ����TIM�ж� //ʹ��
+//	NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;  //TIM3�ж�
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //��ռ���ȼ�0��
+//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;  //�����ȼ�3��
+//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQͨ����ʹ��
+//	NVIC_Init(&NVIC_InitStructure);  //����NVIC_InitStruct��ָ���Ĳ�����ʼ������NVIC�Ĵ���
+
+//	TIM_Cmd(TIM3, ENABLE);  //ʹ��TIMx����
+//							 
+//}
+//extern int SB;
+//void TIM4_IRQHandler(void)   //TIM3�ж�
+//{
+//	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET) //���ָ����TIM�жϷ������:TIM �ж�Դ 
+//		{
+//		TIM_ClearITPendingBit(TIM4, TIM_IT_Update  );  //���TIMx���жϴ�����λ:TIM �ж�Դ 
+//	
+//			if(SB==1)
+//			{
+//				DOOR_OPEN();
+//				SB=0;
+//			}
+//		}
+//}
+
+
+
+
+
+
+
+
+
+
+
+//void TIM2_Int_Init(u16 arr,u16 psc)
+//{
+//  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+//	NVIC_InitTypeDef NVIC_InitStructure;
+
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); //ʱ��ʹ��
+
+//	TIM_TimeBaseStructure.TIM_Period = arr; //��������һ�������¼�װ�����Զ���װ�ؼĴ������ڵ�ֵ	 ������5000Ϊ500ms
+//	TIM_TimeBaseStructure.TIM_Prescaler =psc; //����������ΪTIMxʱ��Ƶ�ʳ�����Ԥ��Ƶֵ  10Khz�ļ���Ƶ��  
+//	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //����ʱ�ӷָ�:TDTS = Tck_tim
+//	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM���ϼ���ģʽ
+//	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure); //����TIM_TimeBaseInitStruct��ָ���Ĳ�����ʼ��TIMx��ʱ�������λ
+// 
+//	TIM_ITConfig( TIM2, TIM_IT_Update ,ENABLE  );//TIM2  //ʹ�ܻ���ʧ��ָ����TIM�ж� //ʹ��
+//	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;  //TIM3�ж�
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //��ռ���ȼ�0��
+//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;  //�����ȼ�3��
+//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQͨ����ʹ��
+//	NVIC_Init(&NVIC_InitStructure);  //����NVIC_InitStruct��ָ���Ĳ�����ʼ������NVIC�Ĵ���
+
+//	TIM_Cmd(TIM2, ENABLE);  //ʹ��TIMx����
+//}
+
+
+//extern int JD;
+//void TIM2_IRQHandler(void)   //TIM3�ж�
+//{
+//	if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) //���ָ����TIM�жϷ������:TIM �ж�Դ 
+//		{
+//			TIM_ClearITPendingBit(TIM2, TIM_IT_Update  );  //���TIMx���жϴ�����λ:TIM �ж�Դ			
+////			WIFI_OPEN_DOOR();
+////			DOOR=1;
+//	if(WIFI1==0)
+//	{		
+////		while(DOOR)
+////		{
+////		DOOR_OPEN();											//WiFi���ų���
+////		}
+//		
+//		
+//			int i;
+//			for(i=0;i<100;i++)
+//			{
+//				JD++;
+//				delay_ms(10);		
+//			}
+//			for(i=100;i>0;i--)
+//			{
+//				JD--;
+//				delay_ms(10);		
+//			}	
+//			JD=0;		
+//		
+//		
+//		
+//		LED0=1;           //2 ding! is ok
+//		delay_ms(100);
+//		LED0=0;
+//		delay_ms(100);		
+//		LED0=1;  		
+//		delay_ms(100);			
+//		LED0=0;
+//		delay_ms(5000);			
+//	}			
+//		}		
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void TIM3_Int_Init(u16 arr,u16 psc)
+{
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); //ʱ��ʹ��
+
+	TIM_TimeBaseStructure.TIM_Period = arr; //��������һ�������¼�װ�����Զ���װ�ؼĴ������ڵ�ֵ	 ������5000Ϊ500ms
+	TIM_TimeBaseStructure.TIM_Prescaler =psc; //����������ΪTIMxʱ��Ƶ�ʳ�����Ԥ��Ƶֵ  10Khz�ļ���Ƶ��  
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //����ʱ�ӷָ�:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM���ϼ���ģʽ
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure); //����TIM_TimeBaseInitStruct��ָ���Ĳ�����ʼ��TIMx��ʱ�������λ
+ 
+	TIM_ITConfig( TIM3, TIM_IT_Update ,ENABLE  );//TIM2  //ʹ�ܻ���ʧ��ָ����TIM�ж� //ʹ��
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;  //TIM3�ж�
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //��ռ���ȼ�0��
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;  //�����ȼ�3��
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQͨ����ʹ��
+	NVIC_Init(&NVIC_InitStructure);  //����NVIC_InitStruct��ָ���Ĳ�����ʼ������NVIC�Ĵ���
+
+	TIM_Cmd(TIM3, ENABLE);  //ʹ��TIMx����
+}
+
+
+
+extern int a11;
+extern int JD;
+void TIM3_IRQHandler(void)   //TIM3�ж�
+{
+	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) //���ָ����TIM�жϷ������:TIM �ж�Դ 
+		{
+			a11++;
+/*************PWM0**************/			
+			if(a11>=(50+JD))
+			{
+				PWM0=0;
+			}
+			if(a11>=2000)
+			{
+				a11=0;
+				PWM0=1;
+			}
+			TIM_ClearITPendingBit(TIM3, TIM_IT_Update  );  //���TIMx���жϴ�����λ:TIM �ж�Դ 			
+		}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
