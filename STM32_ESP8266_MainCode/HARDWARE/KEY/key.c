45601b8f812860b1fbfa0362fe30e6d749004e4e
SunJiYi 666 <53560510+a233a2@users.noreply.github.com>
2022-01-14 11:10:39 +0800

    Create key.c

---
+++ STM32_ESP8266_MainCode/HARDWARE/KEY/key.c
@@ -0,0 +1,62 @@
+#include "key.h"
+#include "delay.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//������ֻ��ѧϰʹ�ã�δ���������ɣ��������������κ���;
+//ALIENTEK Mini STM32������
+//�������� ��������		   
+//����ԭ��@ALIENTEK
+//������̳:www.openedv.com
+//�޸�����:2014/3/06
+//�汾��V1.0
+//��Ȩ���У�����ؾ���
+//Copyright(C) �������������ӿƼ����޹�˾ 2009-2019
+//All rights reserved									   
+//////////////////////////////////////////////////////////////////////////////////	 
+ 	    
+//������ʼ������ 
+//PA15��PC5 ���ó�����
+void KEY_Init(void)
+{
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+ 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOC,ENABLE);//ʹ��PORTA,PORTCʱ��
+
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);//�ر�jtag��ʹ��SWD��������SWDģʽ����
+	
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_15;//PA15
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //���ó���������
+ 	GPIO_Init(GPIOA, &GPIO_InitStructure);//��ʼ��GPIOA15
+	
+	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_5;//PC5
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //���ó���������
+ 	GPIO_Init(GPIOC, &GPIO_InitStructure);//��ʼ��GPIOC5
+ 
+//	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_0;//PA0
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; //PA0���ó����룬Ĭ������	  
+//	GPIO_Init(GPIOA, &GPIO_InitStructure);//��ʼ��GPIOA.0
+	
+} 
+//������������
+//���ذ���ֵ
+//mode:0,��֧��������;1,֧��������;
+//����ֵ��
+//0��û���κΰ�������
+//KEY0_PRES��KEY0����
+//KEY1_PRES��KEY1����
+//WKUP_PRES��WK_UP���� 
+//ע��˺�������Ӧ���ȼ�,KEY0>KEY1>WK_UP!!
+u8 KEY_Scan(u8 mode)
+{	 
+	static u8 key_up=1;//�������ɿ���־
+	if(mode)key_up=1;  //֧������		  
+	if(key_up&&(KEY0==0||KEY1==0))
+	{
+		delay_ms(10);//ȥ���� 
+		key_up=0;
+		if(KEY0==0)return KEY0_PRES;
+		else if(KEY1==0)return KEY1_PRES;
+//		else if(WK_UP==1)return WKUP_PRES; 
+	}else if(KEY0==1&&KEY1==1)key_up=1; 	     
+	return 0;// �ް�������
+}
